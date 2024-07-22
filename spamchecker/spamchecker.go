f972868447ea898e5f8bd03de00813e5e1333142
DJ Tape <Dj.Tapee@yandex.ru>
2024-07-22 03:16:17 +0300

    Version 1.0.0

---
+++ spamchecker/spamchecker.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"cmp"
+	"fmt"
+	"log"
+	"slices"
+	"sync"
+)
+
+func RunPipeline(cmds ...cmd) {
+	in := make(chan interface{})
+	wg := &sync.WaitGroup{}
+	for cmdIdx := range cmds {
+		wg.Add(1)
+		out := make(chan interface{})
+		go func(group *sync.WaitGroup, commandIdx int, in, out chan interface{}) {
+			defer group.Done()
+			defer close(out)
+			cmds[commandIdx](in, out)
+		}(wg, cmdIdx, in, out)
+		in = out
+	}
+	wg.Wait()
+}
+
+func SelectUsers(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
+	uniqueUsers := make(map[string]struct{}, 10)
+	for email := range in {
+		wg.Add(1)
+		go func(group *sync.WaitGroup, email interface{}) {
+			defer group.Done()
+			emailString, ok := email.(string)
+			if !ok {
+				log.Printf("SelectUsers: failed to convert to type: %T", "")
+				return
+			}
+			user := GetUser(emailString)
+			mu.Lock()
+			if _, isAlias := uniqueUsers[user.Email]; !isAlias {
+				uniqueUsers[user.Email] = struct{}{}
+				out <- user
+			}
+			mu.Unlock()
+		}(wg, email)
+	}
+	wg.Wait()
+}
+
+func SelectMessages(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	usersBatch := make([]User, 0, GetMessagesMaxUsersBatch)
+	for user := range in {
+		userStruct, ok := user.(User)
+		if !ok {
+			log.Printf("SelectMessages: failed to convert to type: %T", User{})
+			continue
+		}
+		usersBatch = append(usersBatch, userStruct)
+		if len(usersBatch) < GetMessagesMaxUsersBatch {
+			continue
+		}
+		wg.Add(1)
+		go getMessagesByBatch(wg, slices.Clone(usersBatch), out)
+		usersBatch = usersBatch[:0]
+	}
+	if len(usersBatch) != 0 {
+		wg.Add(1)
+		go getMessagesByBatch(wg, slices.Clone(usersBatch), out)
+	}
+	wg.Wait()
+}
+
+func getMessagesByBatch(group *sync.WaitGroup, batch []User, out chan<- interface{}) {
+	defer group.Done()
+	messages, err := GetMessages(batch...)
+	if err != nil {
+		log.Println("GetMessages func returned error: " + err.Error())
+		return
+	}
+	for _, message := range messages {
+		out <- message
+	}
+}
+
+func CheckSpam(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	for i := 0; i < HasSpamMaxAsyncRequests; i++ {
+		wg.Add(1)
+		go checkSpamWorker(wg, in, out)
+	}
+	wg.Wait()
+}
+
+func checkSpamWorker(group *sync.WaitGroup, in, out chan interface{}) {
+	defer group.Done()
+	for msgID := range in {
+		message, ok := msgID.(MsgID)
+		if !ok {
+			log.Printf("CheckSpam: failed to convert to type: %T", MsgID(0))
+			return
+		}
+		hasSpam, err := HasSpam(message)
+		if err != nil {
+			log.Println("HasSpam func returned error: " + err.Error())
+			return
+		}
+		out <- MsgData{message, hasSpam}
+	}
+}
+
+func CombineResults(in, out chan interface{}) {
+	messages := make([]MsgData, 0)
+	for msgData := range in {
+		messageStruct, ok := msgData.(MsgData)
+		if !ok {
+			log.Printf("CombineResults: failed to convert to type: %T", MsgData{})
+			continue
+		}
+		messages = append(messages, messageStruct)
+	}
+	slices.SortFunc(messages, func(a, b MsgData) int {
+		if a.HasSpam && !b.HasSpam {
+			return -1
+		}
+		if !a.HasSpam && b.HasSpam {
+			return 1
+		}
+		return cmp.Compare(a.ID, b.ID)
+	})
+	for _, msg := range messages {
+		out <- fmt.Sprintf("%t %v", msg.HasSpam, msg.ID)
+	}
+}
